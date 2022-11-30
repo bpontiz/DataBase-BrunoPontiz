316d6b30d50002c894ebf1be4b436cac0541aef7
Bruno Pontiz <bpontiz@gmail.com>
2022-11-30 19:10:26 -0300

    Pushing sensitive data protection.

--- config/mariaDB.js
+++ config/mariaDB.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
 export const options = {
     client: "mysql",
     connection: {
-        host: "",
-        user: "",
-        pass: "",
-        database: ""
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        database: process.env.DB_NAME
     }
 }
