e5bb18cc38eafd83bd8950d2548e766455858153
Bruno Pontiz <bpontiz@gmail.com>
2022-11-30 19:06:41 -0300

    Deleting sensitive data.

--- config/mariaDB.js
+++ config/mariaDB.js
@@ -1,9 +1,9 @@
 export const options = {
     client: "mysql",
     connection: {
-        host: "localhost",
-        user: "root",
-        pass: "r00th0stn0w",
-        database: "ecommerce"
+        host: "",
+        user: "",
+        pass: "",
+        database: ""
     }
 }
